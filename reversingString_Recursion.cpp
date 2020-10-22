6b4dbe5110ae05b2751d19a64554507e585386ea
Abhi-BloodFeeD <abhinavmetta@gmail.com>
2020-10-23 01:31:14 +0530

    recursive 1st

--- reversingString_Recursion.cpp
+++ reversingString_Recursion.cpp
@@ -8,6 +8,13 @@ const int mod = 1e9+7;
 const int mxN = 1e6;
 const int N = 2e5;
 
+void reverseArray(int *arr,int start,int end){
+  if(start>=end)
+  return;
+  swap(*(arr+start),*(arr+end));
+  return reverseArray(arr,++start,--end);
+}
+
 int main() {
   #ifndef ONLINE_JUDGE
   freopen("input.txt","r",stdin);
@@ -16,7 +23,9 @@ int main() {
   int n;cin>>n;
   int arr[n];
   for(int i=0;i<n;i++)cin>>arr[i];
-  
+  reverseArray(arr,0,n-1);
+  for(auto x:arr)cout<<x<<" ";
+  cout<<endl;
   
   
   
